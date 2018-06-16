2c79bec265a3589324cbea15ef9e04dc1e8ec8d2
mimura <kenjiro222@gmail.com>
2018-06-16 10:52:06 +0900

    update

--- webapp/js/script.js
+++ webapp/js/script.js
@@ -1,75 +1,21 @@
 'use strict'
+const MENU =      ["hotcoffe",   "icecoffe",    "hottea",    "icetea",     "cake",  "other"];
+const DISP_MENU = ["ホットコーヒー", "アイスコーヒー", "ホットティー", "アイスティー", "ケーキ", "その他"];
+const MENU_LEN = MENU.length;
 
 let timeLineCount = 0;
+
+
+
 function addAccounts(acc){
-	// acc = {
-	// 	id:"003",
-	// 	persons:3,
-	// 	items : {
-	// 		coffe:3,
-	// 		cake:2
-	// 	}
-	// }
 	timeLineCount++;
-	$("#timeline").append(`
-		<div id="${"acc" + timeLineCount }" class="acc">
-        <div class="persons">
-          <p class="id"> ${acc.id}</p>
-          <div class="display"> 
-            <div class="n"> ${acc.persons} </div>
-            <div class="p"> 名</div>
-          </div>
-        </div>
-        <div class="menu">
-          <div class="item">
-            <div class="name"> Coffe </div> 
-            <div class="wrapb">
-              <div class="down mbtn"> ${acc.items["coffe"] ? acc.items["coffe"] : 0} </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="name">  Cake  </div> 
-            <div class="wrapb">
-              <div class="down mbtn"> ${acc.items["cake"] ? acc.items["cake"] : 0} </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="name">  Tea   </div> 
-            <div class="wrapb">
-              <div class="down mbtn"> ${acc.items["tea"] ? acc.items["tea"] : 0} </div>
-            </div>
-          </div>
-          <div class="item">
-            <div class="name">  Tea   </div> 
-            <div class="wrapb">
-              <div class="down mbtn"> ${acc.items["tea"] ? acc.items["tea"] : 0} </div>
-            </div>
-          </div>
-        </div>
-        <div class="state">
-          <div class="pre btn">
-            <p class="text">あ</p>
-          </div>
-          <div class="now">
-            <p class="text">あ</p>
-          </div>
-          <div class="next btn">
-            <p class="text">あ</p>
-          </div>
-        </div>
-        <div class="operation">
-          <div class="fix btn">
-            <p class="text"> Fix </p>
-          </div>
-          <div class="del btn"> 
-            <p class="text"> Delte  </p>
-          </div>
-        </div>`);
+	$("#timeline").append(makeAccStr(acc.id, acc.persons, acc.items));
 	console.log($(`#${"acc"+timeLineCount}`));
-	$(`#${"acc"+timeLineCount} .btn`).on('click', function(){
+	$(`#${"acc"+timeLineCount} .btn`).on('click', function(e){
 		let id = $(this).parents('.acc').attr("id");
 		let opType = $(this)[0].className.split(" ")[0];
 		handleTimeLineClick(id, opType);
+    e.stopPropagation();
 	});
 	setStatus(`${"acc"+timeLineCount}`,"new");
 }
@@ -133,67 +79,166 @@ function handleTimeLineClick(id, op){
 			break;
 	}
 }
-window.onload = () => {
-	console.log("hello");
-	createSetButton();
+
+function setCreateForm(){
+  $('#createAccount').append(makeCreateAccStr());
+  setCreateButton('#mainOrder');
 }
 
-function createSetButton(){
-	$(`#createAccount .btn, .wrapb`).on('click', function(e){
-		let opType = $(this)[0].className.split(" ")[0];
-		// let id = $(this).parents('.acc').attr("id");
-		handleCreate(opType);
+function setCreateButton(id){
+	$(`${id} .btn`).on('click', function(e){
+    changeItemsCountBy($(this),+1)
+    changeDisplayCount($(this));
 		e.stopPropagation();
 	});
-}
 
-function handleCreate(op){
-	switch(op){
-		case "order":
-			sendMessage(createA());
-			break;
-		default:
-			console.log(`defualt:opType=${op}`);
-	}
-}
+  $(`${id} .wrapb`).on('click', function(e){
+    let node = $(this).parents('.counter');
+    changeItemsCountBy(node,-1); 
+    changeDisplayCount(node);
+    e.stopPropagation();
+  });
 
-// テンプレートを送信する。
-let accid = 0;
-let createA = () => {
-  return {
-    type:'request',
-    op:'create',
-    acc:{
-      id: accid++,
-      date: Date.now(),
-      persons:3,
-      items:{
-        coffe:1,
-        cake:3,
-        tea:1
-      },
-      total:1000,
-      status:'new',//seated,sreved,left,done
+  $(`${id} .order`).on('click', function(e){
+    // console.log(e.target.parents('.acc'));
+    const accNode = $(e.target).parents('.acc');
+    const persons = parseInt(accNode.find('.persons')[0].className.split(" ").getLastVal());
+    let items = {};
+    for(let i = 0; i < MENU_LEN; i++){
+      const itemn = accNode.find(`.items-${MENU[i]}`)[0].className.split(" ").getLastVal();
+      if( itemn === '0'){ continue; }
+      items[MENU[i]] = parseInt(itemn);
+    }
+    const status = 'new';   
+    const sendData = {
+      type:'request',
+      op:'create',
+      acc:{ date:Date.now(), persons, items},
+      status,
       description:'this is test account'
     }
+    console.log(sendData);
+    sendMessage(sendData);
+    e.stopPropagation();
+  }); 
+
+  $(`#createAccount .clear`).on('click', function(e){
+    
+    e.stopPropagation();
+  });    
+}
+ Array.prototype.getLastVal = function (){ return this[this.length -1];}
+
+function changeItemsCountBy(node, diff){
+  console.log(`node=${node} diff=${diff}`);
+  const n = parseInt(node[0].className.split(" ").getLastVal());
+  const nn = n + diff
+  if(nn < 0){
+    console.log(`(n + diff) < 0 => (${n} + ${diff} < 0)`);
+    return;
   }
+  node.removeClass(`${n}`).addClass(`${nn}`);
+  return;
 }
 
-let createB = () => {
-  return {
-    type:'request',
-    op:'create',
-    acc:{
-      id: accid++,
-      date: Date.now(),
-      persons:15,
-      items:{
-        coffe:1,
-        cake:2,
-      },
-      total:1000,
-      status:'new',//seated,sreved,left,done
-      description:'this is test account'
+function changeDisplayCount(node){
+  let n = node[0].className.split(" ").getLastVal();
+  let nnode = node.find('.n');
+  let newstr;
+  if(nnode.text().trim().split(" ")[1])
+    newstr = nnode.text().trim().split(" ")[0] +' '+n;
+  else
+    newstr = n;
+  nnode.text(newstr);
+}
+
+window.onload = () => {
+  setCreateForm();
+}
+
+function makeAccStr(id, persons, items){
+  let str = 
+  `<div id="${"acc" + timeLineCount }" class="acc">
+        <div class="persons">
+          <p class="id"> ${id}</p>
+          <div class="display"> 
+            <div class="n"> ${persons} </div>
+            <div class="p"> 名</div>
+          </div>
+        </div>
+        <div class="menu">`
+  for(let i in items){
+    let disNum = MENU.indexOf(i);
+    console.log(`disNum = ${disNum}`);
+    if(disNum === -1) {
+      console.error(`makeAccStr:make items str Error: item=${i}`);
+      continue;
     }
+    if(items[i] < 1){
+      console.log(`makeAccStr:items(${i} is < 1`);
+      continue;
+    }
+    str += `<div class="item"> 
+              <div class="items-${i} name"> ${DISP_MENU[disNum]}</div> 
+              <div class="wrapb">
+                <div class="down mbtn"> ${items[i]} </div>
+              </div>
+            </div>`
   }
+    str += `
+      </div>
+        <div class="state">
+          <div class="pre btn">
+            <p class="text">あ</p>
+          </div>
+          <div class="now">
+            <p class="text">あ</p>
+          </div>
+          <div class="next btn">
+            <p class="text">あ</p>
+          </div>
+        </div>
+        <div class="operation">
+          <div class="fix btn">
+            <p class="text"> Fix </p>
+          </div>
+          <div class="del btn"> 
+            <p class="text"> Delte  </p>
+          </div>
+        </div>`
+  return str;
+}
+
+function makeCreateAccStr(){
+  let str = `<div class="acc" id="mainOrder">
+        <div class="persons counter p btn 0">
+          <div class="persons wrapb">
+            <div class="down mbtn"> ↓ </div>
+          </div>
+          <div class="display"> 
+            <div class="n"> 0 </div>
+            <div class="p"> 名</div>
+          </div>
+        </div>
+        <div class="menu">`
+  for(let i = 0; i < MENU.length; i++){
+    str += `<div class="items-${MENU[i]} btn item counter 0"> 
+              <div class="items-${MENU[i]} name n"> ${DISP_MENU[i]} 0 </div> 
+              <div class="items-${MENU[i]} wrapb">
+                <div class="down mbtn"> ↓ </div>
+              </div>
+            </div>`
+  }
+  str +=
+        `</div>
+        <div class="operation">
+          <div class="clear clearbtn">
+            <p class="text"> Clear </p>
+          </div>
+          <div class="order orderbtn"> 
+            <p class="text"> 注文!  </p>
+          </div>
+        </div>
+      </div>`
+    return str;
 }
